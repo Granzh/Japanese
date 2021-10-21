66d341855cb552edac9bf9ecdea3ceccb50aecf2
Kirill <92873054+Granzh@users.noreply.github.com>
2021-10-21 19:20:15 +0300

    delete the //, overwritten const

--- japanese.js
+++ japanese.js
@@ -1,17 +1,17 @@
-const japanese_syllable = ["a", "i", "u", "e", "o","ka", "ki", "ku", "ke", "ko","ga", "gi", "gu", "ge", "go","sa", "shi", "su", "se", "so","za", "ji", "zu", "ze", "zo","ta", "chi", "tsu", "te", "to","na", "ni", "nu", "ne", "no"];
-// 35 japanese_symbol.length 
+const JAPANESE_SYLLABLE = ["a", "i", "u", "e", "o","ka", "ki", "ku", "ke", "ko","ga", "gi", "gu", "ge", "go","sa", "shi", "su", "se", "so","za", "ji", "zu", "ze", "zo","ta", "chi", "tsu", "te", "to","na", "ni", "nu", "ne", "no"];
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
-} // Создаём функцию, которая выбирает число от 0 до 3
-function getrandomsymbol() {
-    return japanese_syllable[Math.floor(Math.random() * japanese_syllable.length)]
-} // Выбираем номер слога из массива
+}
+function getRandomSymbol() {
+    return JAPANESE_SYLLABLE[Math.floor(Math.random() * japanese_syllable.length)]
+}
 function getword() {
     let zero;
     let Int = getRandomInt(4)
     let str = '';
     for (zero = -1; zero < Int; zero++) {
-    str += ' ' + getrandomsymbol();}
+    str += ' ' + getRandomSymbol();}
     return str;
 }
 let Ht = document.getElementById('N');    
